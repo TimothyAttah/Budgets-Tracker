6eaa0977bc87965075fb04beb4d6e9f8802cbac3
Timothy Attah <timothyattah733@gmail.com>
2021-08-18 19:28:22 +0100

    Created Profile page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,18 +3,22 @@ import { Router, Route, Switch } from 'react-router-dom';
 import { Header } from './components/header/Header';
 import history from './history';
 import { Home } from './pages/home/Home';
+import { Profile } from './pages/profile/Profile';
 
 export const App = () => {
-  return (
-    <div className="App">
-      <Router history={history}>
-        <Header />
-        <Switch>
-          <Route path='/' exact>
-            <Home />
-          </Route>
-        </Switch>
-     </Router>
-    </div>
-  );
-}
+	return (
+		<div className='App'>
+			<Router history={history}>
+				<Header />
+				<Switch>
+					<Route path='/' exact>
+						<Home />
+					</Route>
+					<Route path='/users/profile/:username'>
+						<Profile />
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+	);
+};
