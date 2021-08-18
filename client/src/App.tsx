721c3942e87014b8a14c779c384314c9f8061383
Timothy Attah <timothyattah733@gmail.com>
2021-08-18 10:22:27 +0100

    Added and created MainHeader components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import history from './history';
+import { Home } from './pages/home/Home';
 
 export const App = () => {
   return (
     <div className="App">
-     <h2>Hello world</h2>
+      <Router history={history}>
+        <Switch>
+          <Route path='/' exact>
+            <Home />
+          </Route>
+        </Switch>
+     </Router>
     </div>
   );
 }
---
+++ client/src/pages/home/Home.tsx
@@ -0,0 +1,10 @@
+import { MainHeader } from "../../components/header/MainHeader"
+
+export const Home = () => {
+  return (
+    <div>
+      <MainHeader />
+      <h1>Home Page</h1>
+    </div>
+  )
+}
