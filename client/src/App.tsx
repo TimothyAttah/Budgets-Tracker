7f1d5e397975c1066b6062dc43a356101df350c8
Timothy Attah <timothyattah733@gmail.com>
2021-09-24 12:44:14 +0100

    Initial setups

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,51 +1,9 @@
-import React from 'react';
-import { Router, Route, Switch } from 'react-router-dom';
-import { Header } from './components/header/Header';
-import history from './history';
-import { Home } from './pages/home/Home';
-import { Notes } from './pages/notes/Notes';
-import { NotesCreate } from './pages/notes/NotesCreate';
-import { Profile } from './pages/profile/Profile';
-import { Signin } from './pages/user/Signin';
-import { Signup } from './pages/user/Signup';
+import React from 'react'
 
 export const App = () => {
-	return (
-		<>
-			<Router history={history}>
-				<Header />
-				<Switch>
-					{/* <Route path='/' exact>
-						<Home />
-					</Route>
-					<Route path='/users/profile/:username'>
-						<Profile />
-					</Route>
-					<Route path='/users/notes' exact>
-						<Notes />
-					</Route>
-					<Route path='/users/notes/create/note' component={NotesCreate} />
-					<Route path='/users/notes/create/note'>
-						<NotesCreate />
-					</Route>
-					<Route path='/users/signup'>
-						<Signup />
-					</Route>
-					<Route path='/users/signin'>
-						<Signin />
-					</Route> */}
-					<Route path='/' exact component={Home} />
-					<Route path='/users/signin' exact component={Signin} />
-					<Route path='/users/signup' exact component={Signup} />
-					<Route path='/users/notes' exact component={Notes} />
-					<Route
-						path='/users/notes/create/note'
-						exact
-						component={NotesCreate}
-					/>
-					<Route path='/users/profile/:username' exact component={Profile} />
-				</Switch>
-			</Router>
-		</>
-	);
-};
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  )
+}
--- client/src/redux/types.ts
+++
@@ -1,7 +0,0 @@
-export enum NotesTypes {
-  NOTE_CREATE = 'NOTE_CREATE',
-  NOTES_LISTS = 'NOTES_LISTS',
-  NOTE_LIST = 'NOTE_LIST',
-  NOTE_DELETE = 'NOTE_DELETE',
-  NOTE_EDIT = 'NOTE_EDIT',
-}
