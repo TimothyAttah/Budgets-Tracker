ec73e3601907c2a00a19acabc95505f5bb35b74f
Timothy Attah <timothyattah733@gmail.com>
2021-09-25 11:54:14 +0100

    Reducers and actions set ups for incomes and expenses

--- client/src/redux/reducers/index.ts
+++ client/src/redux/reducers/index.ts
@@ -1,5 +1,15 @@
 import { combineReducers } from 'redux';
+import { Incomes, Expenses } from '../reduxInterface';
+
+import { incomes } from './income';
+import { expenses } from './expenses';
+
+export interface StoreState {
+  incomes: Incomes;
+  expenses: Expenses;
+}
 
 export const reducers = combineReducers({
-  hello: ()=> 1+1
-})
+  incomes,
+  expenses
+});
