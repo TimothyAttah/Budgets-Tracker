9e0e988e04d2e8e23e6d5d6173b0310b8217a0f8
Timothy Attah <timothyattah733@gmail.com>
2021-09-26 18:44:50 +0100

    change header position

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -8,7 +8,12 @@ export const HeaderContainer = styled.div`
 	justify-content: space-between;
 	align-items: center;
 	position: fixed;
+	max-width: 1500px;
 	width: 100%;
+	margin: auto;
+	top: 0;
+	left: 0;
+	right: 0;
 	background: var(--clr-accent);
 	color: var(--clr-light);
 	padding: 20px;
