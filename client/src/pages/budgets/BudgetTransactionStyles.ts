c321b7db5f500ad0bb396ee4344da11ac05056c0
Timothy Attah <timothyattah733@gmail.com>
2021-09-28 00:30:47 +0100

    Calculated all transactions

---
+++ client/src/pages/budgets/BudgetTransactionStyles.ts
@@ -0,0 +1,108 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+	width: 80%;
+	display: flex;
+	flex-direction: column;
+	color: var(--clr-light);
+	justify-content: center;
+	align-items: center;
+	flex-wrap: wrap;
+	margin: auto;
+	text-align: center;
+
+	h2 {
+		width: 50%;
+		margin-top: 50px;
+		margin-bottom: 80px;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		flex-wrap: wrap;
+		text-align: center;
+	}
+
+	.income {
+		background-color: green;
+		color: var(--clr-light);
+		padding: 10px 20px;
+		width: 50%;
+		font-size: 1.5rem;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin-bottom: 20px;
+	}
+	.expense {
+		background-color: red;
+		color: var(--clr-light);
+		padding: 10px 20px;
+		width: 50%;
+		font-size: 1.5rem;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		flex-wrap: wrap;
+		margin-bottom: 15px;
+	}
+	@media (max-width: 1000px) {
+		h2 {
+			width: 60%;
+		}
+	}
+	@media (max-width: 770px) {
+		h2 {
+			font-size: 2rem;
+		}
+		h2,
+		.income,
+		.expense {
+			flex-direction: row;
+			width: 70%;
+		}
+		h2 {
+			span {
+				padding-top: 0px;
+			}
+		}
+	}
+	@media (max-width: 600px) {
+		h2 {
+			font-size: 1.5rem;
+		}
+		.income,
+		.expense {
+			font-size: 1.2rem;
+		}
+	}
+	@media (max-width: 420px) {
+		h2,
+		.income,
+		.expense {
+			flex-direction: column;
+		}
+		h2 {
+			span {
+				padding-top: 15px;
+			}
+		}
+	}
+	@media (max-width: 320px) {
+		h2 {
+			font-size: 1.4rem;
+		}
+		.income,
+		.expense {
+			font-size: 1rem;
+		}
+	}
+	@media (max-width: 280px) {
+		h2 {
+			font-size: 1.2rem;
+		}
+		.income,
+		.expense {
+			font-size: 0.9rem;
+		}
+	}
+`;
--- client/src/pages/budgets/BudgetsTransaction.tsx
+++ client/src/pages/budgets/BudgetsTransaction.tsx
@@ -1,124 +1,69 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { listExpenses } from '../../redux/actions/expenses';
+import { listIncome } from '../../redux/actions/income';
+import { StoreState } from '../../redux/reducers';
 
-const Container = styled.div`
-	width: 80%;
-	display: flex;
-	flex-direction: column;
-	color: var(--clr-light);
-	justify-content: center;
-	align-items: center;
-	flex-wrap: wrap;
-	margin: auto;
-	text-align: center;
-
-	h2 {
-		width: 50%;
-		margin-top: 50px;
-		margin-bottom: 80px;
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		flex-wrap: wrap;
-		text-align: center;
-	}
-
-	.income {
-		background-color: green;
-		color: var(--clr-light);
-		padding: 10px 20px;
-		width: 50%;
-		font-size: 1.5rem;
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		margin-bottom: 20px;
-	}
-	.expense {
-		background-color: red;
-		color: var(--clr-light);
-		padding: 10px 20px;
-		width: 50%;
-		font-size: 1.5rem;
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		flex-wrap: wrap;
-		margin-bottom: 15px;
-	}
-	@media (max-width: 1000px) {
-		h2 {
-			width: 60%;
-		}
-	}
-	@media (max-width: 770px) {
-		h2 {
-			font-size: 2rem;
-		}
-		h2,
-		.income,
-		.expense {
-			flex-direction: row;
-			width: 70%;
-		}
-		h2 {
-			span {
-				padding-top: 0px;
-			}
-		}
-	}
-	@media (max-width: 600px) {
-		h2 {
-			font-size: 1.5rem;
-		}
-		.income,
-		.expense {
-			font-size: 1.2rem;
-		}
-	}
-	@media (max-width: 420px) {
-		h2,
-		.income,
-		.expense {
-			flex-direction: column;
-		}
-		h2 {
-			span {
-				padding-top: 15px;
-			}
-		}
-	}
-	@media (max-width: 320px) {
-		h2 {
-			font-size: 1.4rem;
-		}
-		.income,
-		.expense {
-			font-size: 1rem;
-		}
-	}
-	@media (max-width: 280px) {
-		h2 {
-			font-size: 1.2rem;
-		}
-		.income,
-		.expense {
-			font-size: .9rem;
-		}
-	}
-`;
+import { Container } from './BudgetTransactionStyles';
 
 export const BudgetsTransaction = () => {
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(listExpenses());
+		dispatch(listIncome());
+	},[dispatch])
+	const { expenses } = useSelector((state: StoreState) => state.expenses);
+	const { incomes } = useSelector((state: StoreState) => state.incomes);
+
+	const incomeTransactions = incomes.map(income => income.value);
+	const expensesTransactions = expenses.map(expense => expense.value);
+	const totalIncomesValues = incomeTransactions.reduce((val, results) => (val += results), 0).toFixed(2);
+	const totalExpensesValues = expensesTransactions.reduce((val, results) => (val += results), 0).toFixed(2);
+	const totalIncomes = parseInt(totalIncomesValues);
+	const totalExpenses = parseInt(totalExpensesValues);
+	const totalBalance = totalIncomes - totalExpenses;
+
+
   return (
 		<Container>
 			<h2>
-				Balance: <span>+ 123,456,790.00</span>
+				Balance:
+				<span>
+					{totalBalance > 1
+						? `+ ${totalBalance
+								.toString()
+								.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`
+						: `${totalBalance
+								.toString()
+								.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`}
+					.00
+				</span>
 			</h2>
 			<h4 className='income'>
-				Incomes: <span>+ 123,456,790.00</span>
+				Incomes:
+				<span>
+					{totalIncomes > 1
+						? `+ ${totalIncomes
+								.toString()
+								.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`
+						: `${totalIncomes
+								.toString()
+								.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`}
+					.00
+				</span>
 			</h4>
 			<h4 className='expense'>
-				Expenses: <span> - 123,456,790.00</span>
+				Expenses:{' '}
+				<span>
+					{totalExpenses > 1
+						? `- ${totalExpenses
+								.toString()
+								.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`
+						: `${totalExpenses
+								.toString()
+								.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`}
+					.00
+				</span>
 			</h4>
 		</Container>
 	);
