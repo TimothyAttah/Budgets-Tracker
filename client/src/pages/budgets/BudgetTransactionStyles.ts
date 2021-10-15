7a8150f9e8ccec4beefc9075bc3047128a3b3086
Timothy Attah <timothyattah733@gmail.com>
2021-10-15 10:59:58 +0100

    Clean up Budget.jsx

--- client/src/pages/budgets/BudgetTransactionStyles.ts
+++ client/src/pages/budgets/BudgetTransactionStyles.ts
@@ -30,6 +30,7 @@ export const Container = styled.div`
 		font-size: 2.5rem;
 		letter-spacing: 3px;
 	}
+
 	h4 {
 		color: #322a2ab8;
 		padding: 15px 20px;
@@ -40,6 +41,7 @@ export const Container = styled.div`
 		align-items: center;
 		margin-bottom: 20px;
 		position: relative;
+
 		span {
 			color: #fff;
 			position: absolute;
@@ -53,13 +55,13 @@ export const Container = styled.div`
 			padding: 5px 10px;
 		}
 	}
-
 	.income {
 		background: #008074;
 	}
 	.expense {
 		background-color: #ff5049;
 	}
+
 	@media (max-width: 1210px) {
 		h2 {
 			font-size: 2.8rem;
@@ -67,21 +69,19 @@ export const Container = styled.div`
 			letter-spacing: 2px;
 		}
 	}
+
 	@media (max-width: 540px) {
 		h2 {
 			font-size: 2.5rem;
-			margin: 20px 0;
-			letter-spacing: 2px;
 		}
 	}
+
 	@media (max-width: 420px) {
 		p {
 			font-size: 1.2rem;
 		}
 		h2 {
 			font-size: 2.3rem;
-			margin: 20px 0;
-			letter-spacing: 2px;
 		}
 		h4 {
 			margin-bottom: 10px;
@@ -94,6 +94,7 @@ export const Container = styled.div`
 			}
 		}
 	}
+
 	@media (max-width: 320px) {
 		p {
 			font-size: 1.2rem;
@@ -114,81 +115,5 @@ export const Container = styled.div`
 			}
 		}
 	}
-
-	.body__container {
-		color: #555;
-		font-family: Open Sans;
-		font-size: 16px;
-		height: 100vh;
-		font-weight: 4;
-		position: relative;
-	}
-	.top__containers {
-		height: 40vh;
-		background-image: linear-gradient(rgba(0, 0, 0, 0.35), rgba(0, 0, 0, 0.35)),
-			url(back.png);
-		background-size: cover;
-		background-position: center;
-		position: relative;
-	}
-	/* @media (max-width: 1000px) {
-		h2 {
-			width: 60%;
-		}
-	}
-	@media (max-width: 770px) {
-		h2 {
-			font-size: 2rem;
-		}
-		h2,
-		.income,
-		.expense {
-			flex-direction: row;
-			width: 70%;
-		}
-		h2 {
-			span {
-				padding-top: 0px;
-			}
-		}
-	}
-	@media (max-width: 600px) {
-		h2 {
-			font-size: 1.5rem;
-		}
-		.income,
-		.expense {
-			font-size: 1.2rem;
-		}
-	}
-	@media (max-width: 420px) {
-		h2,
-		.income,
-		.expense {
-			flex-direction: column;
-		}
-		h2 {
-			span {
-				padding-top: 15px;
-			}
-		}
-	}
-	@media (max-width: 320px) {
-		h2 {
-			font-size: 1.4rem;
-		}
-		.income,
-		.expense {
-			font-size: 1rem;
-		}
-	}
-	@media (max-width: 280px) {
-		h2 {
-			font-size: 1.2rem;
-		}
-		.income,
-		.expense {
-			font-size: 0.9rem;
-		}
-	} */
+	
 `;
