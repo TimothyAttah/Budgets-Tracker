ac228e0bae425860878e427f3519a72b9f8bc9a5
Timothy Attah <timothyattah733@gmail.com>
2021-11-08 23:08:36 +0100

    change font-wegight

--- client/src/pages/budgets/ExpensesList.tsx
+++ client/src/pages/budgets/ExpensesList.tsx
@@ -17,7 +17,7 @@ export const Container = styled.div`
 	h2 {
 		color: var(--clr-red);
 		font-size: 1.5rem;
-		font-weight: 200;
+		font-weight: 300;
 	}
 `;
 
--- client/src/pages/budgets/IncomeList.tsx
+++ client/src/pages/budgets/IncomeList.tsx
@@ -17,7 +17,7 @@ export const Container = styled.div`
 	h2 {
 		color: var(--clr-green);
 		font-size: 1.5rem;
-		font-weight: 200;
+		font-weight: 300;
 	}
 `;
 
